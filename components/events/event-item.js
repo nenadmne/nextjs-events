5dd4522017da17388a39db2c6737d9efff3fc947
nenadmne <cosovicnenad14@gmail.com>
2023-11-24 15:37:03 +0100

    Button moved inside content div

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
-import Link from "next/link";
-import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AdressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import classes from "./event-item.module.css";
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -20,15 +22,22 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AdressIcon />
             <adress>{formattedAdress}</adress>
           </div>
         </div>
-      </div>
-      <div className={classes.actions}>
-        <Button link={`/events/${id}`}> Explore event </Button>
+        <div className={classes.actions}>
+          <Button link={`/events/${id}`}>
+            <span>Explore event</span>{" "}
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
       </div>
     </li>
   );
