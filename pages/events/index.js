ac152aa79c2ef41108c08af551fb4f72acf38454
nenadmne <cosovicnenad14@gmail.com>
2023-11-25 13:53:04 +0100

    Adding event list

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,11 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+
 export default function Events() {
-    return (
-      <div>
-        <h1> Event list </h1>
-      </div>
-    );
-  }
-  
+  const events = getAllEvents();
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+}
