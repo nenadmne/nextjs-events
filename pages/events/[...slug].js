250420b1f7c1cb23860f37c87703b9ff2b1f58ab
nenadmne <cosovicnenad14@gmail.com>
2023-11-26 22:14:27 +0100

    Setting up event filter page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,33 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
 export default function FilteredEvents() {
-  return (
-    <div>
-      <h1> Filtered Events</h1>
-    </div>
-  );
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center"> Loading... </p>;
+  }
+
+  const month = +filterData[0];
+  const year = +filterData[1];
+
+  if (
+    isNaN(month) ||
+    isNaN(year) ||
+    year > 2025 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ){
+    return <p> Invalid filtered data </p>
+  }
+
+  const filteredEvents = getFilteredEvents()
+    return (
+      <div>
+        <h1> Filtered Events</h1>
+      </div>
+    );
 }
