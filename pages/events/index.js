e2d4c25e27d2dba02d586365479638eba357e048
nenadmne <cosovicnenad14@gmail.com>
2023-12-02 19:03:32 +0100

    Replacing events dummyData with firebase data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,46 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
-import { getAllEvents } from "../../dummy-data";
 
-export default function Events() {
-  const events = getAllEvents();
+export default function Events({ events }) {
   const router = useRouter();
 
   const searchHandler = (month, year) => {
     const fullPath = `/events/${year}/${month}`;
-
     router.push(fullPath);
   };
-  
+
   return (
     <div>
       <EventSearch onSearch={searchHandler} />
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-js-events-ceee0-default-rtdb.europe-west1.firebasedatabase.app/events.json"
+  );
+
+  const responseData = await response.json();
+
+  const events = [];
+
+  for (const key in responseData) {
+    events.push({
+      id: key,
+      title: responseData[key].title,
+      description: responseData[key].description,
+      location: responseData[key].location,
+      date: responseData[key].date,
+      image: responseData[key].image,
+      isFeatured: responseData[key].isFeatured,
+    });
+  }
+  return {
+    props: {
+      events: events,
+    },
+  };
 }
