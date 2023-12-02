98d88848028c91aca39d75d0a0415b588863b642
nenadmne <cosovicnenad14@gmail.com>
2023-12-02 19:18:04 +0100

    Geting event by Id from firebase

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,10 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/error-alert/error-alert";
 
-export default function SingleEvent() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+export default function SingleEvent({ event }) {
   if (!event) {
     return (
       <ErrorAlert>
@@ -32,4 +26,63 @@ export default function SingleEvent() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    "https://next-js-events-ceee0-default-rtdb.europe-west1.firebasedatabase.app/events.json"
+  );
+  const responseData = await response.json();
+
+  const events = [];
+
+  for (const key in responseData) {
+    events.push({
+      id: key,
+      title: responseData[key].title,
+      description: responseData[key].description,
+      location: responseData[key].location,
+      date: responseData[key].date,
+      image: responseData[key].image,
+      isFeatured: responseData[key].isFeatured,
+    });
+  }
+
+  const event = events.find((item) => item.id === params.eventId);
+  return {
+    props: {
+      event: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await fetch(
+    "https://next-js-events-ceee0-default-rtdb.europe-west1.firebasedatabase.app/events.json"
+  );
+  const responseData = await response.json();
+
+  const events = [];
+
+  for (const key in responseData) {
+    events.push({
+      id: key,
+      title: responseData[key].title,
+      description: responseData[key].description,
+      location: responseData[key].location,
+      date: responseData[key].date,
+      image: responseData[key].image,
+      isFeatured: responseData[key].isFeatured,
+    });
+  }
+
+  const eventId = events.map((item) => ({
+    params: { eventId: item.id },
+  }));
+
+  return {
+    paths: eventId,
+    fallback: false,
+  };
 }
