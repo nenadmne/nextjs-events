f52306807cf94ce8603deb1ef8a908a0545acd11
nenadmne <cosovicnenad14@gmail.com>
2023-12-03 17:55:24 +0100

    Added meta that will be default meta for all pages that dont have meta defined, if there is meta defined it will replace this one

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,12 @@ import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  //Meta with name - viewport is usualy added to Head so it ensures app will working better in responsive mode
   return (
     <Layout>
       <Head>
+        <title> NextJs Events</title>
+        <meta name="description" content="nextJs event application" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
