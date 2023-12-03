5feda350d69c20737aaf14cdb2f474ff2e59e3ff
nenadmne <cosovicnenad14@gmail.com>
2023-12-03 17:37:01 +0100

    Adding the Head content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../dummy-data";
 
 export default function Homepage({ featuredEvents }) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allows you to evolve..."
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
